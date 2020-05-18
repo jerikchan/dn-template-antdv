de7d472505d8759d02cbb7576d4fad21c49c0868
jerikchan <jerikchan@gmail.com>
2020-05-19 03:30:33 +0800

    first commit

---
+++ src/faicomponent.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+// import { Button } from 'ant-design-vue';
+import Button from 'ant-design-vue/es/button';
+import 'ant-design-vue/es/button/style';
+
+Vue.use(Button);
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './assets/common.less';
+import Vue from 'vue';
+import App from './components/App.vue';
+import './faicomponent';
+
+new Vue({
+  el: '#root',
+  render: h => h(App)
+});
